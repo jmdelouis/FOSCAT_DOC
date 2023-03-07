c2bf07f21952673aaec68758a2df83c29d708d3f
jd22d11 <jmdeloui@ifremer.fr>
2023-03-07 18:07:02 +0100

    remove reference to teh template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FoSCAT'
+copyright = '2023, Delouis'
+author = 'Delouis'
 
-release = '0.1'
-version = '0.1.0'
+release = '2.0'
+version = '2.0.0'
 
 # -- General configuration
 
